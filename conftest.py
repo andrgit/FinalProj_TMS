ffdcff399f1ad74561cffea7024eec70d2d81d1d
andrgit <86713245+andrgit@users.noreply.github.com>
2022-02-10 22:38:14 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -10,18 +10,6 @@ from selenium import webdriver
 #     yield driver
 #     driver.quit()
 
-# @pytest.fixture(scope='session')
-# def driver():
-#     options = webdriver.ChromeOptions()
-#     options.add_argument('--no-sandbox')
-#     options.add_argument('--headless')
-#     options.add_argument('--disable-gpu')
-#     options.add_argument("--remote-debugin-port=9222")
-#     options.add_argument("--screen-size=1200x800")
-#     driver = webdriver.Chrome(options=options)
-#     driver.maximize_window()
-#     driver.implicitly_wait(5)
-#     yield driver
 
 @pytest.fixture(scope='session')
 def driver():
